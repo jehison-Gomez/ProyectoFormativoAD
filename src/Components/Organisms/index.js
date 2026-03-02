9ed42368d016ff4e0ee95a90499ec2ad4a98a2f3
adriana776 <treview227@gmail.com>
2026-03-02 09:49:21 -0500

    organismos atomic design

---
+++ src/Components/Organisms/index.js
@@ -0,0 +1,3 @@
+export { default as UserList } from './UserList/UserList'
+export { default as UserSection } from './UserSection/UserSection'
+export { default as ProfileWithActions } from './ProfileWithActions/ProfileWithActions'
