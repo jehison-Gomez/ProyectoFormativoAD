a3f077b5d62158bc1efd4fd7f0dd3b5ea2d59c8d
El-dany001 <daniel1stive@gmail.com>
2026-03-09 13:59:05 -0500

    Subo nueva version del proyecto

--- src/Components/Atoms/Icon/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Icon({ icon: IconCompo, className = "" }) {
-  return (
-    <div className={className}>
-      <IconCompo />
-    </div>
-  )
-}
