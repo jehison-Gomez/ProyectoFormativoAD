86cdf1eb71a3b54d6ccafe1d84272bbf7671209a
victorlozada34 <victormanuelcerqueralozada@gmail.com>
2026-06-03 01:32:58 -0500

    expandir BadgeEstado con más estados

--- src/Components/Atoms/BadgeEstado/BadgeEstado.jsx
+++ src/Components/Atoms/BadgeEstado/BadgeEstado.jsx
@@ -7,6 +7,9 @@ export const BadgeEstado = ({ estado }) => {
     return (
         <span className={`${styles.badge} ${isActivo ? styles.activo : styles.inactivo}`}>
             {estado}
+
+
+            
         </span>
     );
 };
